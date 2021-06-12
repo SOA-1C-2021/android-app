372b55931eed668e4a71313fc529f14c8ad0636d
Sergio Amoruso <sergio.amoruso@southworks.com>
2021-06-12 10:23:48 -0300

    Agregar logica patron de desbloqueo

--- codigo/app/src/main/java/com/soa/app/AuthActivity.java
+++ codigo/app/src/main/java/com/soa/app/AuthActivity.java
@@ -2,22 +2,75 @@ package com.soa.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.andrognito.patternlockview.PatternLockView;
+import com.andrognito.patternlockview.listener.PatternLockViewListener;
+import com.andrognito.patternlockview.utils.PatternLockUtils;
+import com.andrognito.patternlockview.utils.ResourceUtils;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
+
 public class AuthActivity extends AppCompatActivity {
+
+    private TextView textWelcome = null;
+    private PatternLockView patternLockView = null;
+    private final String neededPattern = "678543012";
+    private Snackbar incorrectPatternSnackbar = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_auth);
 
-        TextView textWelcome = findViewById(R.id.text_welcome);
+        // reference ui components
+        textWelcome = findViewById(R.id.text_welcome);
+        patternLockView = (PatternLockView) findViewById(R.id.pattern_lock_view);
 
+        // create snackbar
+        incorrectPatternSnackbar = Snackbar.make(findViewById(android.R.id.content).getRootView(), "Patrón incorrecto", Snackbar.LENGTH_SHORT);
+
+        // add listeners
+        patternLockView.addPatternLockListener(patternLockViewListener);
 
     }
+
+    private PatternLockViewListener patternLockViewListener = new PatternLockViewListener() {
+        @Override
+        public void onStarted() {
+            Log.d(getClass().getName(), "Pattern drawing started");
+        }
+
+        @Override
+        public void onProgress(List<PatternLockView.Dot> progressPattern) {
+            Log.d(getClass().getName(), "Pattern progress: " +
+                    PatternLockUtils.patternToString(patternLockView, progressPattern));
+        }
+
+        @Override
+        public void onComplete(List<PatternLockView.Dot> pattern) {
+            String stringPattern = PatternLockUtils.patternToString(patternLockView, pattern);
+            Log.d(getClass().getName(), "Pattern complete: " + stringPattern);
+
+            if (stringPattern.equals(neededPattern)) {
+                Log.d(getClass().getName(), "Correct pattern entered");
+                patternLockView.setCorrectStateColor(ResourceUtils.getColor(AuthActivity.this, R.color.green));
+                Intent loginActivityIntent = new Intent(AuthActivity.this, LoginActivity.class);
+                startActivity(loginActivityIntent);
+            } else {
+                Log.d(getClass().getName(), "Incorrect pattern entered");
+                patternLockView.setCorrectStateColor(ResourceUtils.getColor(AuthActivity.this, R.color.red));
+                incorrectPatternSnackbar.show();
+            }
+        }
+
+        @Override
+        public void onCleared() {
+            Log.d(getClass().getName(), "Pattern has been cleared");
+        }
+    };
 }
