ff2355ff37aada721149b46c14b634e28c5bbca9
Sergio Amoruso <sergio.amoruso@southworks.com>
2021-06-15 16:41:19 -0300

    Agregar estado de carga de bateria en pantalla inicial

--- codigo/app/src/main/java/com/soa/app/AuthActivity.java
+++ codigo/app/src/main/java/com/soa/app/AuthActivity.java
@@ -2,10 +2,15 @@ package com.soa.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.andrognito.patternlockview.PatternLockView;
 import com.andrognito.patternlockview.listener.PatternLockViewListener;
@@ -20,7 +25,7 @@ public class AuthActivity extends AppCompatActivity {
     private TextView textWelcome = null;
     private PatternLockView patternLockView = null;
     private final String neededPattern = "678543012";
-    private Snackbar incorrectPatternSnackbar = null;
+    private TextView textBatteryStatus = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +35,13 @@ public class AuthActivity extends AppCompatActivity {
         // reference ui components
         textWelcome = findViewById(R.id.text_welcome);
         patternLockView = (PatternLockView) findViewById(R.id.pattern_lock_view);
-
-        // create snackbar
-        incorrectPatternSnackbar = Snackbar.make(findViewById(android.R.id.content).getRootView(), "Patrón incorrecto", Snackbar.LENGTH_SHORT);
+        textBatteryStatus = findViewById(R.id.text_battery_status);
 
         // add listeners
         patternLockView.addPatternLockListener(patternLockViewListener);
+
+        // register battery status receiver
+        registerReceiver(this.batteryStatusReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
 
     }
 
@@ -64,13 +70,23 @@ public class AuthActivity extends AppCompatActivity {
             } else {
                 Log.d(getClass().getName(), "Incorrect pattern entered");
                 patternLockView.setCorrectStateColor(ResourceUtils.getColor(AuthActivity.this, R.color.red));
-                incorrectPatternSnackbar.show();
+                Toast.makeText(AuthActivity.this, "Patrón incorrecto", Toast.LENGTH_SHORT).show();
             }
         }
 
         @Override
         public void onCleared() {
             Log.d(getClass().getName(), "Pattern has been cleared");
+        }
+    };
+
+    private BroadcastReceiver batteryStatusReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context ctxt, Intent intent) {
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+            int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+            float batteryPct = level * 100 / (float) scale;
+            textBatteryStatus.setText("Estado de carga de la batería: " + String.valueOf(batteryPct) + "%");
         }
     };
 }
